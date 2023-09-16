34d5ff53b14d4306c36ee8812b5bd054139962f4
liber09 <linda.bergsangel@hotmail.com>
2023-09-16 06:24:28 +0200

    Created getProductById and Working on modifyProduct

--- src/main/java/service/Warehouse.java
+++ src/main/java/service/Warehouse.java
@@ -3,15 +3,44 @@ package service;
 import entities.Category;
 import entities.Product;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Warehouse {
-    public Product AddProduct(String name, Category category, int rating){
+    private List<Product> products = new ArrayList<>();
+    public boolean AddProduct(String name, Category category, int rating) {
         if(name.trim().isEmpty()){
-            System.out.println("Can not add products without name");
-            return null;
+            System.out.println("Can't add products without name");
+
+            return false;
         }
+
         if (category == null){
             category = Category.OTHER;
         }
-        return new Product(name,category,rating);
+
+        Product newProduct = new Product(name,category,rating);
+        products.add(newProduct);
+
+        return true;
+    }
+    public Optional<Product> getProductById(int id) {
+        return products.stream()
+                .filter(p -> p.getId().equals(id)).findFirst();
+    }
+
+    public boolean modifyProduct(int id, String name, Category category, int rating) {
+        Optional<Product> product = getProductById(id);
+
+        if (!product.isPresent()) {
+            return false;
+        } else {
+            if (!name.isEmpty()){
+
+            }
+        }
+
+        return true;
     }
 }
